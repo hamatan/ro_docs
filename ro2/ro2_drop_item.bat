3784a88000785fc8fc2356dad5125c10474c0662
hamatan <oxygenvx@gmail.com>
2021-02-26 02:00:32 +0900

    add mob map

--- ro2/ro2_drop_item.bat
+++ ro2/ro2_drop_item.bat
@@ -3,7 +3,7 @@ echo off)
 setlocal enableextensions
 set "THIS_PATH=%~f0"
 set "PARAM_1=%~1"
-PowerShell.exe -windowstyle hidden -Command "iex -Command ((gc \"%THIS_PATH:`=``%\") -join \"`n\")"
+PowerShell.exe -Command "iex -Command ((gc \"%THIS_PATH:`=``%\") -join \"`n\")"
 exit /b %errorlevel%
 -- この1つ上の行までバッチファイル
 ') | sv -Name TempVar
@@ -61,23 +61,36 @@ foreach( $work in $mob ){  `
 
 #############################################################
 # ドロップ率をアイテム名に含める
+Measure-Command{ `
+$newtable = @()
 foreach($work in $ro2){ `
-	if($work.drop_per1 -ne 0){ $work.drop_item1 += " ("+ 0.01* $work.drop_per1 +"%)" }; `
-	if($work.drop_per2 -ne 0){ $work.drop_item2 += " ("+ 0.01* $work.drop_per2 +"%)" }; `
-	if($work.drop_per3 -ne 0){ $work.drop_item3 += " ("+ 0.01* $work.drop_per3 +"%)" }; `
-	if($work.drop_per4 -ne 0){ $work.drop_item4 += " ("+ 0.01* $work.drop_per4 +"%)" }; `
-	if($work.drop_per5 -ne 0){ $work.drop_item5 += " ("+ 0.01* $work.drop_per5 +"%)" }; `
-	if($work.drop_per6 -ne 0){ $work.drop_item6 += " ("+ 0.01* $work.drop_per6 +"%)" }; `
-	if($work.drop_per7 -ne 0){ $work.drop_item7 += " ("+ 0.01* $work.drop_per7 +"%)" }; `
-	if($work.drop_per8 -ne 0){ $work.drop_item8 += " ("+ 0.01* $work.drop_per8 +"%)" }`
+#	$data = New-Object PSObject | Select-Object ID, NAME, MAP, ITEM1, ITEM2, ITEM3, ITEM4, ITEM5, ITEM6, ITEM7, ITEM8;
+
+#	$data.ID   = $work.ID;
+#	$data.NAME = $work.NAME;
+
+	$newtable.add($work.ID   + ",");
+	$newtable.add($work.NAME + ",");
+
+	if($work.drop_per1 -ne 0){ $data.ITEM1 = $work.drop_item1 + " ("+ 0.01* $work.drop_per1 +"%)" }else{ $data.ITEM1 = $work.drop_item1	}; `
+	if($work.drop_per2 -ne 0){ $data.ITEM2 = $work.drop_item2 + " ("+ 0.01* $work.drop_per2 +"%)" }else{ $data.ITEM2 = $work.drop_item2	}; `
+	if($work.drop_per3 -ne 0){ $data.ITEM3 = $work.drop_item3 + " ("+ 0.01* $work.drop_per3 +"%)" }else{ $data.ITEM3 = $work.drop_item3	}; `
+	if($work.drop_per4 -ne 0){ $data.ITEM4 = $work.drop_item4 + " ("+ 0.01* $work.drop_per4 +"%)" }else{ $data.ITEM4 = $work.drop_item4	}; `
+	if($work.drop_per5 -ne 0){ $data.ITEM5 = $work.drop_item5 + " ("+ 0.01* $work.drop_per5 +"%)" }else{ $data.ITEM5 = $work.drop_item5	}; `
+	if($work.drop_per6 -ne 0){ $data.ITEM6 = $work.drop_item6 + " ("+ 0.01* $work.drop_per6 +"%)" }else{ $data.ITEM6 = $work.drop_item6	}; `
+	if($work.drop_per7 -ne 0){ $data.ITEM7 = $work.drop_item7 + " ("+ 0.01* $work.drop_per7 +"%)" }else{ $data.ITEM7 = $work.drop_item7	}; `
+	if($work.drop_per8 -ne 0){ $data.ITEM8 = $work.drop_item8 + " ("+ 0.01* $work.drop_per8 +"%)" }else{ $data.ITEM8 = $work.drop_item8	}; `
 
 # マップ名にモンスター名とドロップ率を追加
 	$i = [array]::IndexOf($mob.ID, $work.ID); `
 	if( $i -ne -1){
-		$work.drop_per1 = $mob[$i].MAP; `
+		$data.MAP = $mob[$i].MAP; `
 	} `
+	$newtable += $data;
 }
-
+}
 #############################################################
 # GUIで表示
-$ro2 | select-object name,drop_per1,drop_item1,drop_item2,drop_item3,drop_item4,drop_item5,drop_item6,drop_item7,drop_item8 | Out-GridView -wait
+#$newtable | select-object NAME,MAP, ITEM1, ITEM2, ITEM3, ITEM4, ITEM5, ITEM6, ITEM7, ITEM8 | Out-GridView -wait
+
+$map_id,$map_name
