8792a29658a3fcbe6e10026086a5364aa0d57a08
hamatan <oxygenvx@gmail.com>
2021-02-26 03:20:05 +0900

    add mob count

--- ro2/ro2_drop_item.bat
+++ ro2/ro2_drop_item.bat
@@ -3,7 +3,7 @@ echo off)
 setlocal enableextensions
 set "THIS_PATH=%~f0"
 set "PARAM_1=%~1"
-PowerShell.exe -Command "iex -Command ((gc \"%THIS_PATH:`=``%\") -join \"`n\")"
+PowerShell.exe -windowstyle hidden -Command "iex -Command ((gc \"%THIS_PATH:`=``%\") -join \"`n\")"
 exit /b %errorlevel%
 -- この1つ上の行までバッチファイル
 ') | sv -Name TempVar
@@ -46,9 +46,10 @@ else{
 # マップ名を抽出
 $map_id  = New-Object 'System.Collections.Generic.List[System.String]';
 $map_name= New-Object 'System.Collections.Generic.List[System.String]';
+$mob_cnt = New-Object 'System.Collections.Generic.List[System.String]';
 
 foreach( $work in $map ){ `
-   $map_id.Add($work.ID);
+   $map_id.Add($work.ID); `
    $map_name.Add($work.MAP_NAME);
 }
 
@@ -56,41 +57,43 @@ foreach( $work in $mob ){  `
    $i = [array]::IndexOf($map_id, $work.MAP); `
    if( $i -ne -1){
       $work.MAP = $map_name[$i]; `
+      $mob_cnt.Add($work.count);
+   }
+   else{
+      $mob_cnt.Add( "" );
    }
 }
 
 #############################################################
-# ドロップ率をアイテム名に含める
-Measure-Command{ `
-$newtable = @()
+#Measure-Command{ `
+$newtable = New-Object 'System.Collections.Generic.List[System.String]';
 foreach($work in $ro2){ `
 #	$data = New-Object PSObject | Select-Object ID, NAME, MAP, ITEM1, ITEM2, ITEM3, ITEM4, ITEM5, ITEM6, ITEM7, ITEM8;
 
 #	$data.ID   = $work.ID;
 #	$data.NAME = $work.NAME;
 
-	$newtable.add($work.ID   + ",");
-	$newtable.add($work.NAME + ",");
-
-	if($work.drop_per1 -ne 0){ $data.ITEM1 = $work.drop_item1 + " ("+ 0.01* $work.drop_per1 +"%)" }else{ $data.ITEM1 = $work.drop_item1	}; `
-	if($work.drop_per2 -ne 0){ $data.ITEM2 = $work.drop_item2 + " ("+ 0.01* $work.drop_per2 +"%)" }else{ $data.ITEM2 = $work.drop_item2	}; `
-	if($work.drop_per3 -ne 0){ $data.ITEM3 = $work.drop_item3 + " ("+ 0.01* $work.drop_per3 +"%)" }else{ $data.ITEM3 = $work.drop_item3	}; `
-	if($work.drop_per4 -ne 0){ $data.ITEM4 = $work.drop_item4 + " ("+ 0.01* $work.drop_per4 +"%)" }else{ $data.ITEM4 = $work.drop_item4	}; `
-	if($work.drop_per5 -ne 0){ $data.ITEM5 = $work.drop_item5 + " ("+ 0.01* $work.drop_per5 +"%)" }else{ $data.ITEM5 = $work.drop_item5	}; `
-	if($work.drop_per6 -ne 0){ $data.ITEM6 = $work.drop_item6 + " ("+ 0.01* $work.drop_per6 +"%)" }else{ $data.ITEM6 = $work.drop_item6	}; `
-	if($work.drop_per7 -ne 0){ $data.ITEM7 = $work.drop_item7 + " ("+ 0.01* $work.drop_per7 +"%)" }else{ $data.ITEM7 = $work.drop_item7	}; `
-	if($work.drop_per8 -ne 0){ $data.ITEM8 = $work.drop_item8 + " ("+ 0.01* $work.drop_per8 +"%)" }else{ $data.ITEM8 = $work.drop_item8	}; `
+# ドロップ率をアイテム名に含める
+	if($work.drop_per1 -ne 0){ $item1 = $work.drop_item1 + " ("+ 0.01* $work.drop_per1 +"%)" }else{ $item1 = $work.drop_item1 }; `
+	if($work.drop_per2 -ne 0){ $item2 = $work.drop_item2 + " ("+ 0.01* $work.drop_per2 +"%)" }else{ $item2 = $work.drop_item2 }; `
+	if($work.drop_per3 -ne 0){ $item3 = $work.drop_item3 + " ("+ 0.01* $work.drop_per3 +"%)" }else{ $item3 = $work.drop_item3 }; `
+	if($work.drop_per4 -ne 0){ $item4 = $work.drop_item4 + " ("+ 0.01* $work.drop_per4 +"%)" }else{ $item4 = $work.drop_item4 }; `
+	if($work.drop_per5 -ne 0){ $item5 = $work.drop_item5 + " ("+ 0.01* $work.drop_per5 +"%)" }else{ $item5 = $work.drop_item5 }; `
+	if($work.drop_per6 -ne 0){ $item6 = $work.drop_item6 + " ("+ 0.01* $work.drop_per6 +"%)" }else{ $item6 = $work.drop_item6 }; `
+	if($work.drop_per7 -ne 0){ $item7 = $work.drop_item7 + " ("+ 0.01* $work.drop_per7 +"%)" }else{ $item7 = $work.drop_item7 }; `
+	if($work.drop_per8 -ne 0){ $item8 = $work.drop_item8 + " ("+ 0.01* $work.drop_per8 +"%)" }else{ $item8 = $work.drop_item8 }; `
 
 # マップ名にモンスター名とドロップ率を追加
 	$i = [array]::IndexOf($mob.ID, $work.ID); `
 	if( $i -ne -1){
-		$data.MAP = $mob[$i].MAP; `
-	} `
-	$newtable += $data;
+		$newtable.add( $work.ID + "," + $work.NAME + "," + "["+ $mob_cnt[$i] +"] / " + $mob[$i].MAP + "," + $item1 + "," + $item2 + "," + $item3 + "," + $item4 + "," + $item5 + "," + $item6 + "," + $item7 + "," + $item8 );
+	}
+	else{
+		$newtable.add( $work.ID + "," + $work.NAME + "," + ""                                       + "," + $item1 + "," + $item2 + "," + $item3 + "," + $item4 + "," + $item5 + "," + $item6 + "," + $item7 + "," + $item8 );
+	}
+#	$newtable += $data;
 }
-}
+#}
 #############################################################
 # GUIで表示
-#$newtable | select-object NAME,MAP, ITEM1, ITEM2, ITEM3, ITEM4, ITEM5, ITEM6, ITEM7, ITEM8 | Out-GridView -wait
-
-$map_id,$map_name
+$newtable | ConvertFrom-Csv -Header "ID", "NAME", "MAP", "ITEM1", "ITEM2", "ITEM3", "ITEM4", "ITEM5", "ITEM6", "ITEM7", "ITEM8" | Out-GridView -wait
